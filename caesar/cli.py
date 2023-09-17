51e6563c4fa4ba85d4dae7c7997fe6d80e4214b9
AmeriCodes <mateus-americo@live.com>
2023-09-17 15:35:16 -0300

    refactor: linting

--- caesar/cli.py
+++ caesar/cli.py
@@ -1,14 +1,14 @@
 from argparse import ArgumentParser
+
 from caesar import decripta, encripta
 
 # Live de Python 36: https://youtu.be/LwxVscJUapk
 
-parser = ArgumentParser(description='Cifra de Cesar')
-parser.add_argument('frase', help='Frase a ser enciptada/decriptada', type=str)
-parser.add_argument(
-    '-n', help='Valor de rotação', default=13, type=int, required=False
-)
-parser.add_argument('-d', help='Decripta', required=False, action='store_true')
+parser = ArgumentParser(description="Cifra de Cesar")
+parser.add_argument("frase", help="Frase a ser enciptada/decriptada", type=str)
+parser.add_argument("-n", help="Valor de rotação", default=13, type=int, required=False)
+parser.add_argument("-d", help="Decripta", required=False, action="store_true")
+
 
 def cli():
     args = parser.parse_args()
@@ -17,8 +17,8 @@ def cli():
     else:
         resultado = encripta(args.frase, args.n)
 
-    print(f'Entrada: {args.frase}')
-    print(f'Saida: {resultado}')
+    print(f"Entrada: {args.frase}")
+    print(f"Saida: {resultado}")
 
 
 cli()
